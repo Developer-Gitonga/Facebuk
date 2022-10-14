6198a29f3efaa111dd44af8df36cee6ceecaef0b
Develper-Gitonga <aizensalim52@gmail.com>
2022-10-14 17:50:39 +0300

    ch:boilerplate series

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
-import { getSession } from 'next-auth/client'
+import { unstable_getServerSession } from 'next-auth'
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { authOptions } from "./auth/[...nextauth]"
 
-export default function Home({ session }) {
-  if (!session) return <Login />
+
+export default function Home() {
+  // if (!session) return <Login />
   
   return (
     <div>
@@ -25,13 +28,27 @@ export default function Home({ session }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  // Get the user
-  const session = await getSession(context);
+// export async function getServerSideProps(context) {
+//   // Get the user
+//   const session = await getSession(context);
 
-  return {
-    props: {
-      session
-    }
+//   return {
+//     props: {
+//       session
+//     }
+//   }
+// }
+
+export async (req, res) => {
+  const session = await unstable_getServerSession(req, res, authOptions)
+  if (session) {
+    res.send({
+      content:
+        "This is protected content. You can access this content because you are signed in.",
+    })
+  } else {
+    res.send({
+      error: "You must be signed in to view the protected content on this page.",
+    })
   }
 }
