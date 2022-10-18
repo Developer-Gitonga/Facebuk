4a7cfd1a5278acb20f9b552756a19f033f87e2b6
Develper-Gitonga <aizensalim52@gmail.com>
2022-10-18 22:34:21 +0300

    ch: feature

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Post() {
   return (
-    <div>Post</div>
-  )
+    <div>
+      <div>
+        <div>
+          <Image />
+        </div>
+        <div>
+          <p>{name}</p>
+          <p></p>
+              </div>
+              <p>{message}</p>
+          </div>
+          {postImage && (
+              <div>
+                  <Image/>
+              </div>
+          )}
+    </div>
+  );
 }
 
 export default Post
