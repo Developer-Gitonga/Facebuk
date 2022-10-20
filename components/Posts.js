f016619389c059cd37d8594f194871db1bfbd603
Develper-Gitonga <aizensalim52@gmail.com>
2022-10-20 12:08:06 +0300

    ch: post feature

---
+++ components/Posts.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useCollection } from "react-firebase-hooks"
+import { db } from "../firebase"
+import Post from './Post'
+
+function Posts() {
+    const [realtimePosts, loading, error] = useCollection(
+        db.collection("posts").orderBy("timestamp", "desc")
+    )
+    return (
+    <div>
+        {realtimePosts.docs.map((post) => (
+                    <Post
+                        key={post.id}
+                        name={post.data().name}
+                        message={post.data().message}
+                        email={post.data().email}
+                        timestamp={post.data().timestamp}
+                        image={post.data().image}
+                        postImage={post.data().Posts}
+                    />   
+        ))}
+    </div>
+  )
+}
+
+export default Posts
